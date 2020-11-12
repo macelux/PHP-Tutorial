831843c2419fe242ebc61f704a8e06b34a268109
Mac <macelux@hotmail.com>
2020-11-12 14:50:54 -0800

    echo statement

--- index.php
+++ index.php
@@ -3,12 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php Script</title>
+    <title>Php For Beginners</title>
 </head>
 <body>
     <?php 
     // code goes here
-    echo "I love cats";
+    echo "I love cats";  
+    // echo is not case sensitive
+    echo "i love dogs";
+    ECHO "I love dogs";
+
+    // output HTML tags
+    echo "<h1> this is a heading </h1> <br>";
+    echo "<h2> this is a heading </h2><br>";
+    echo "<h3> this is a heading </h3><br>";
+
+    // output variables
+    $name = "Mac Chika";
+    echo $name;
+    echo "<p>My name is $name <br>";
+    echo "<p>My name is ".$name."<br>";
+    echo 123;
+    echo 0.9;
+    echo $name . 2 . 'string';
     ?>
 </body>
 </html>
